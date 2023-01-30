e5776fa84d2bc8226a8919edb154f8a7e64e5129
3tawi <96850409+3tawi@users.noreply.github.com>
2023-01-30 16:38:25 +0100

    Update SmartMatrix_esp32_serial_rx.ino

--- SmartMatrix_esp32_serial_rx/SmartMatrix_esp32_serial_rx.ino
+++ SmartMatrix_esp32_serial_rx/SmartMatrix_esp32_serial_rx.ino
@@ -1,16 +1,18 @@
 /*
-// - Lien vidéo: https://youtu.be/FNpEdO0jpaU
+// - Lien vidéo: https://youtu.be/nAiwnpTsgsY
 //   REQUIRES the following Arduino libraries:
 // - SmartMatrix Library: https://github.com/pixelmatix/SmartMatrix
+// - Adafruit_GFX Library: https://github.com/adafruit/Adafruit-GFX-Library
 */
 
+#define USE_ADAFRUIT_GFX_LAYERS
 #include <MatrixHardware_ESP32_V0.h>                // This file contains multiple ESP32 hardware configurations, edit the file to define GPIOPINOUT (or add #define GPIOPINOUT with a hardcoded number before this #include)
 #include <SmartMatrix.h>
 
 #define COLOR_DEPTH 24                  // Choose the color depth used for storing pixels in the layers: 24 or 48 (24 is good for most sketches - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24)
 const uint16_t kMatrixWidth = 64;       // Set to the width of your display, must be a multiple of 8
 const uint16_t kMatrixHeight = 64;      // Set to the height of your display
-const uint8_t kRefreshDepth = 36;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
+const uint8_t kRefreshDepth = 24;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
 const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save RAM, more to keep from dropping frames and automatically lowering refresh rate.  (This isn't used on ESP32, leave as default)
 const uint8_t kPanelType = SM_PANELTYPE_HUB75_64ROW_MOD32SCAN;   // Choose the configuration that matches your panels.  See more details in MatrixCommonHub75.h and the docs: https://github.com/pixelmatix/SmartMatrix/wiki
 const uint32_t kMatrixOptions = (SM_HUB75_OPTIONS_NONE);        // see docs for options: https://github.com/pixelmatix/SmartMatrix/wiki
@@ -19,35 +21,42 @@ const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
 SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
 SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
 
-#define UpHeader 0x9C
-#define endHeader 0x36
+#define UpHeader 0x01
+#define endHeader 0x03
 
 Stream* mySeriel;
 uint32_t lastData = 1000;
-const uint16_t NUM_LEDS = kMatrixWidth * kMatrixHeight + 1;
+const uint16_t NUM_LEDS = kMatrixWidth * kMatrixHeight;
+uint16_t buff[NUM_LEDS];
 
+IRAM_ATTR void IRQ_HANDLER(void *);
+
+void Drawframe(void);
 
 void setDriver(Stream* s) {
   mySeriel = s;
 }
 
-void drawFrame() {
+void Drawframe() {
   if (mySeriel->read() != UpHeader)
        return;
+  mySeriel->readBytes((uint8_t *)buff, NUM_LEDS*2);
   rgb24 *buffer = backgroundLayer.backBuffer();
-  mySeriel->readBytes((uint8_t *)buffer, NUM_LEDS*3);
+  for (int i=0; i<NUM_LEDS; i++)
+    buffer[i] = rgb16(buff[i]);
   if (mySeriel->read() != endHeader)
        return;
   backgroundLayer.swapBuffers();
 }
 
 void setup() {
-  Serial.begin(1350000);
+  Serial.begin(1300000);
   setDriver(&Serial);
   delay(5000);
   matrix.addLayer(&backgroundLayer); 
   matrix.begin();
   backgroundLayer.setBrightness(255);
+  backgroundLayer.setFont(font3x5);
   //backgroundLayer.enableColorCorrection(true);
 }
 
@@ -55,7 +64,7 @@ void loop() {
   if (mySeriel->available() > 0) {
     switch (mySeriel->peek()) {
       case UpHeader:
-         drawFrame();
+         Drawframe();
          lastData = millis();
          break;
       default:
@@ -63,9 +72,8 @@ void loop() {
          break;
     }
   }
-  else if (millis() - lastData > 3000) {
+  if (millis() - lastData > 3000) {
       backgroundLayer.fillScreen({ 0, 0, 0 });
-      backgroundLayer.setFont(font3x5);
       backgroundLayer.drawString(3, 24, { 255, 0, 255 }, "Waiting");
       backgroundLayer.swapBuffers();
       lastData = millis();
